10c99c6a5f3b15987eb0ba4ba28e98fd462840de
Seyed Ali Dehghan <alizzz6006@gmail.com>
2022-01-02 20:08:58 +0330

    little changes on bootstrap

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,6 +2,7 @@ import "./TodoList.css";
 import { useReducer, useContext } from "react";
 import { Container, Button, Row, Col } from "react-bootstrap";
 import { data, reducer, types, TodoListContext } from "..";
+import AddIcon from '../../asset/icon/plus.png';
 
 function TodoList() {
   const { todoList, dispatch } = useContext(TodoListContext);
@@ -28,43 +29,49 @@ function TodoList() {
       <Button size="lg" onClick={() => handleAddTodo()}>
         Add
       </Button>
-      <Row className="justify-content-center">
-        <Col xs={6}>
-            {todoList.map((todo) => (
-              <div className="todo" key={todo.id}>
-                <h2>{todo.name}</h2>
-                <p>{todo.description}</p>
-                <ul>
-                  {todo.items.map((item) => (
-                    <li key={item.id}>
-                      <input
-                        onChange={(e) =>
-                          handleChangeItemStatus(todo.id, item.id, e)
-                        }
-                        style={{ marginRight: "0.2rem" }}
-                        type="checkbox"
-                        checked={item.status}
-                        id={todo.id + item.id}
-                      ></input>
-                      <label htmlFor={todo.id + item.id}>{item.title}</label>
-                    </li>
-                  ))}
-                </ul>
-                <Button
-                  style={{ width: "100%", marginBottom: "5px" }}
-                  onClick={() => handleAddTodoItem(todo.id)}
-                >
-                  Add item
-                </Button>
-                <Button
-                  style={{ width: "100%" }}
-                  onClick={() => handleDeleteTodo(todo.id)}
-                >
-                  Delete
-                </Button>
-              </div>
-            ))}
+      <Row className="mt-2">
+        <Col  xs={8} sm={6} lg={2}>
+          <div className="todo" style={{width:"100%",height:"100%",color:"#647989",display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <img src={AddIcon} style={{width: "30%",height:"30%"}} alt="" />
+          </div>
         </Col>
+
+        {todoList.map((todo) => (
+          <Col xs={8} sm={6} lg={2}>
+            <div className="todo mt-2" key={todo.id}>
+              <h2>{todo.name}</h2>
+              <p>{todo.description}</p>
+              <ul>
+                {todo.items.map((item) => (
+                  <li key={item.id}>
+                    <input
+                      onChange={(e) =>
+                        handleChangeItemStatus(todo.id, item.id, e)
+                      }
+                      style={{ marginRight: "0.2rem" }}
+                      type="checkbox"
+                      checked={item.status}
+                      id={todo.id + item.id}
+                    ></input>
+                    <label htmlFor={todo.id + item.id}>{item.title}</label>
+                  </li>
+                ))}
+              </ul>
+              <Button
+                style={{ width: "100%", marginBottom: "5px" }}
+                onClick={() => handleAddTodoItem(todo.id)}
+              >
+                Add item
+              </Button>
+              <Button
+                style={{ width: "100%" }}
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                Delete
+              </Button>
+            </div>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
